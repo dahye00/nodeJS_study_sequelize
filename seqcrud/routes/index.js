89f4b05e414421b804db635300a9c6fb4540672f
DESKTOP-7IETLFJ\임다혜 <devldh@naver.com>
2019-01-08 01:31:50 +0900

    데이터 추가하기

--- seqcrud/routes/index.js
+++ seqcrud/routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const models = require('../models');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +9,22 @@ router.get('/', function(req, res, next) {
 
 router.get('/show', function(req, res, next) {
     res.render('show');
+})
+
+router.post('/create', function(req, res, next) {
+    let body = req.body;
+
+    models.post.create({
+        title: body.inputTitle,
+        writer: body.inputWriter
+    })
+    .then(result => {
+        console.log('데이터 추가 완료');
+        res.redirect('/show');
+    }).catch(err=>{
+        console.log('데이터 추가 실패');
+        console.log(err);
+    })
 })
 
 module.exports = router;
