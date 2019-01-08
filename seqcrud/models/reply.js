af1364c995be7fd1c4bd20e7bbd4bafeaec9cac5
DESKTOP-7IETLFJ\임다혜 <devldh@naver.com>
2019-01-08 22:57:58 +0900

    댓글 model 생성

---
+++ seqcrud/models/reply.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const reply = sequelize.define('reply', {
+    postId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    writer: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    }
+  });
+  return reply;
+};
