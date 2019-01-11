a00f5c90415a27153d9f7c50ca1a513c5d275864
DESKTOP-7IETLFJ\임다혜 <devldh@naver.com>
2019-01-11 18:46:47 +0900

    비밀번호 암호화

--- seqcrud/routes/member.js
+++ seqcrud/routes/member.js
@@ -3,6 +3,10 @@ const models = require('../models');
 const crypto = require('crypto');
 const router = express.Router();
 
+router.get('/', function(req, res, next) {
+    res.send('환영합니다');
+})
+
 router.get('/sign_up', function(req, res, next) {
     res.render('member/signup');
 });
@@ -26,6 +30,37 @@ router.post('/sign_up', function(req, res, next) {
     .catch(err => {
         console.log(err);
     })
+})
+
+router.get('/login', function(req, res, next) {
+    res.render('member/login');
+})
+
+router.post('/login', function(req, res, next) {
+    let body = req.body;
+
+    models.member.find({
+        where: {email : body.memberEmail}
+    })
+    .then(result => {
+        let dbPassword = result.dataValues.password;
+
+        let inputPassword = body.password;
+        let salt = result.dataValues.salt;
+        let hashPassword = crypto.createHash('sha512').update(inputPassword + salt).digest('hex');
+
+        if(dbPassword === hashPassword) {
+            console.log('비밀번호 일치');
+            res.redirect('/member');
+        } else {
+            console.log('비밀번호 불일치');
+            res.redirecct('/member/login');
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
 })
 
 module.exports = router;
